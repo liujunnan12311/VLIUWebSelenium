1715bd6a63236f64e9debbb6b4783b2607ea0f0d
Aaron Li <azli@advent.com>
2016-06-15 02:06:23 -0700

    Optimized run.bat, removed unused line

--- bin/run.bat
+++ bin/run.bat
@@ -1,5 +1,3 @@
-set JAVA_OPTS=%JAVA_OPTS% -DuserNumber=20 -Dduration=15 -Dserver=vmw12r2apxapi.gencos.com -DuserName=admin -Dpassword=advs
-
 @echo off
 dir ..\user-files\simulations\APX_Cloud /b /a-d>Tests.txt
 
